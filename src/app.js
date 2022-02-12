7935a75afaccb855e6103c4f973bc5024350aeeb
Clovis <clovis@dnclovis.com>
2022-02-12 12:43:59 -0600

    Initialize top and left navbar

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-import React, { Fragment } from 'react';
+import React, {Fragment, useState,} from 'react';
 
-const App = () => (
-    <React.StrictMode>
-        <Fragment>
-            Hello World!!!
-        </Fragment>
-    </React.StrictMode>
-);
+import SideNavBar from "./components/navbar/sideNavBar";
+import TopNavBar from "./components/navbar/topNavBar";
+
+
+const App = () => {
+    const toggleMenuClass = 'toggle-sidebar';
+    const [className, setClassName ] = useState('');
+    const handleToggleMenuClick = (e) => {
+        (className === toggleMenuClass)? setClassName('') : setClassName(toggleMenuClass);
+        e.preventDefault();
+    };
+    return (
+        <React.StrictMode>
+            <Fragment>
+                <div className={className}>
+                    <TopNavBar handleToggleMenu={handleToggleMenuClick}/>
+                    <SideNavBar/>
+                </div>
+            </Fragment>
+        </React.StrictMode>
+
+    );
+}
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -6,4 +6,15 @@ import ReactDOM from 'react-dom';
 
 import App from './app';
 
+
+// import "./vendor/bootstrap/css/bootstrap.min.css"
+import "./vendor/bootstrap-icons/bootstrap-icons.css"
+import "./vendor/bootstrap-icons/bootstrap-icons.css"
+// import "./vendor/quill/quill.snow.css"
+// import "./vendor/quill/quill.bubble.css"
+// import "./vendor/remixicon/remixicon.css"
+// import "./vendor/simple-datatables/style.css"
+
+import "./index.css";
+
 ReactDOM.render(<App />, document.getElementById('root'));
