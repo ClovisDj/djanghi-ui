ec92e3a35085c0832c6773951ed0d92067a3b94e
Clovis <clovis@dnclovis.com>
2022-02-18 12:15:28 -0600

    Customize association menu

--- src/components/navbar/sideNavBar.js
+++ src/components/navbar/sideNavBar.js
@@ -3,23 +3,26 @@ import {Fragment} from "react";
 
 const SideNavBar = ({user, association}) => {
     const userIsAdmin = (user && user.attributes.hasOwnProperty("is_admin")) ? user.attributes.is_admin : false;
+    const associationFields = association ? association.attributes.member_contribution_fields : [];
+
+    const associationFieldLiElements = (
+        associationFields.map((field, index) => (
+            <li key={index}>
+                <a><i className="bi bi-circle"></i><span>{field.name}</span></a>
+            </li>
+            )
+        )
+    );
+
     const associationMenu = (userIsAdmin &&
         <li className="nav-item" style={{cursor: "pointer"}}>
             <a className="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse">
                 <i className="bi bi-menu-button-wide"></i><span>Association Menu</span><i
                 className="bi bi-chevron-down ms-auto"></i>
             </a>
+
             <ul id="components-nav" className="nav-content collapse " data-bs-parent="#sidebar-nav">
-                <li>
-                    <a>
-                        <i className="bi bi-circle"></i><span>Alerts</span>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                        <i className="bi bi-circle"></i><span>Accordion</span>
-                    </a>
-                </li>
+                {associationFieldLiElements}
             </ul>
         </li>
     );
