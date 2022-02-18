f920857a8afae9e8c692030ef46d3d8adc341689
Clovis <clovis@dnclovis.com>
2022-02-18 11:40:19 -0600

    Add 404 component

---
+++ src/components/404.js
@@ -0,0 +1,32 @@
+import {Fragment} from "react";
+import {useNavigate} from "react-router-dom";
+
+
+const NotFoundPage = () => {
+    let navigate = useNavigate();
+
+    const backHome = (event) => {
+        event.preventDefault();
+        navigate("/", { replace: true});
+    };
+
+    return (
+        <Fragment>
+            <main>
+                <div className="container">
+
+                    <section
+                        className="section error-404 min-vh-100 d-flex flex-column align-items-center justify-content-center">
+                        <h1>404</h1>
+                        <h2>The page you are looking for doesn't exist.</h2>
+                        <a className="btn" onClick={backHome}>Back to home</a>
+                        <img src={process.env.PUBLIC_URL + "/images/404.svg"} className="img-fluid py-5" alt="Page Not Found" />
+                    </section>
+
+                </div>
+            </main>
+        </Fragment>
+    );
+};
+
+export default NotFoundPage;
