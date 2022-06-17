84216df3f6103a90ee67dfc443ed4f3769666d04
Clovis <diclovis@gmail.com>
2022-06-17 17:49:55 -0500

    Hard code host endpoint

--- src/utils/apiConfiguration.js
+++ src/utils/apiConfiguration.js
@@ -4,7 +4,7 @@ import TokenManager from "./authToken";
 const tokenManager = new TokenManager();
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_HOST ? process.env.REACT_APP_API_HOST : "http://localhost:8000"
+  baseURL: process.env.REACT_APP_API_HOST ? process.env.REACT_APP_API_HOST : "https://api.djanghi.com"
 });
 
 axiosInstance.interceptors.request.use((config) => {
