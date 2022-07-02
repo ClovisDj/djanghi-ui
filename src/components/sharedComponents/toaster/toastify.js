58178be6553c9d988405c228bd43186283e54962
Clovis <diclovis@gmail.com>
2022-07-01 19:00:42 -0500

    Profile section done

---
+++ src/components/sharedComponents/toaster/toastify.js
@@ -0,0 +1,44 @@
+import {toast, ToastContainer} from "react-toastify";
+
+
+const CustomToaster = ({}) => {
+    return (
+        <ToastContainer position="top-right"
+                        autoClose={2000}
+                        hideProgressBar
+                        newestOnTop={false}
+                        closeOnClick
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                        pauseOnHover
+        />
+    );
+};
+
+export const successToast = (message = "") => {
+    const successMessage = "🎉" + message;
+    toast.success(successMessage, {
+        position: "top-right",
+        autoClose: 2500,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
+};
+
+export const errorToast = (message = "An unexpected error occurred !") => {
+    toast.error(message, {
+        position: "top-right",
+        autoClose: 2500,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
+};
+
+export default CustomToaster;
--- src/components/userProfile/index.js
+++ src/components/userProfile/index.js
@@ -1,6 +1,9 @@
 import {Fragment} from "react";
 
+import "react-toastify/dist/ReactToastify.css";
+
 import "./index.css"
+import 'react-phone-number-input/style.css'
 import BaseUserProfile from "./base";
 import BaseDashboard from "../dashboard/base";
 
